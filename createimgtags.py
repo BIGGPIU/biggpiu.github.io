46718c9fc855de98f3f57af28fa9d23180ac7b95
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2024-07-02 14:57:45 -0400

    updated createimagetags, added fallback image, updated picker page to show the chart name

--- createimgtags.py
+++ createimgtags.py
@@ -4,7 +4,15 @@ def main(path,diff):
     hold = os.listdir(path)
     with open("temp.txt","w",encoding='utf-8') as f:
         for i in hold:
-            f.write(f"<img src='charts/{diff}/{i}' alt='{i}' class='songimage'>\n")
+            if ".png" in i:
+                songname = i.replace(".png","")
+            elif ".jpg" in i:
+                songname = i.replace(".jpg","")
+            f.write(f"""
+                    <div class="songimage">
+                        <img src='charts/{diff}/{i}' alt='{i}' class="cardsongimage"  onerror="this.onerror=null; this.src='charts/fallback.png';"">
+                        <div class="songtext">{songname}</div>
+                    </div>""")
 
 
 if __name__ == "__main__":
