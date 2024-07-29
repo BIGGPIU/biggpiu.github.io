bd75f2e28d17207bd2be31fb0e72ecc681d1a001
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2024-07-29 14:21:35 -0400

    Added prices tab

---
+++ Thecave/Pricing/scrollreact.js
@@ -0,0 +1,13 @@
+var loadme = document.getElementsByClassName("rhythmgametier")
+window.onscroll = function() {show()};
+let i = 0
+let x = 100
+console.log(loadme)
+
+function show () {
+    if (document.documentElement.scrollTop > x) {
+        loadme[i].setAttribute("class","rhythmgametier show")
+        i++ 
+        x += 250
+    }
+}
