8917888172f073256b8726ad2c2ae434e105c584
BIGGPIU <114438660+BIGGPIU@users.noreply.github.com>
2026-03-02 17:14:45 -0500

    added a button that slows down your page alot if you press it

---
+++ pressme/main.js
@@ -0,0 +1,17 @@
+
+    function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    };
+
+    function FUCKME() {
+        let bs_vec = []
+        let shrimp_hanger = document.getElementById("bazinga")
+        while (true) {
+            bs_vec.push(
+                "20 Trillion to israel"
+            )
+            console.log(bs_vec);
+            let append_html = '<img src="https://static.vecteezy.com/system/resources/thumbnails/042/235/995/small/ai-generated-shrimp-isolated-on-transparent-background-png.png" alt="greetings lesbian"> </img>'
+            $( shrimp_hanger ).append(append_html);
+        }
+    }
